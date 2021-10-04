b32f4a5017b867bad02cea0e726cf9b4c327f1be
AnushkaSinha2020 <75123533+AnushkaSinha2020@users.noreply.github.com>
2021-10-04 17:41:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+video = "";
+status = "";
+function preload(){
+    video = createVideo('video.mp4');
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function setup(){
+    canvas= createCanvas(480, 380);
+    canvas.position(500, 280);
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
